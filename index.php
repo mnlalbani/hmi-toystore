c69ace12b93ed05856f3bc77941ca9d2b876f289
Manuel Albani <mnlalbani@gmail.com>
2017-03-11 12:01:41 -0400

    added logout functionality and profile link to index.

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+    session_start();
+    if(isset($_GET['logout'])){
+        session_unset();
+        session_destroy();
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -49,7 +56,14 @@
                                 </div>
                             </form>
                         </li>
-                        <li><a href="login.php"><i class="material-icons left tooltipped" data-position="bottom" data-delay="50" data-tooltip="Ingresa">account_circle</i><span class="hide-on-med-and-down">Ingresa</span></a></li>
+                        <?php
+                            if(isset($_SESSION['correo'])){
+                            echo "<li><a href='perfil.php'><i class='material-icons left tooltipped' data-position='bottom' data-delay='50' data-tooltip='Perfil'>account_circle</i><span class='hide-on-med-and-down'>Perfil</span></a></li>";
+                            echo "<li><a href='index.php?logout=true'><i class='material-icons left tooltipped' data-position='bottom' data-delay='50' data-tooltip='Salir'>exit_to_app</i><span class='hide-on-med-and-down'>Salir</span></a></li>";
+                            }else{
+                            echo "<li><a href='login.php'><i class='material-icons left tooltipped' data-position='bottom' data-delay='50' data-tooltip='Ingresa'>account_circle</i><span class='hide-on-med-and-down'>Ingresa</span></a></li>";    
+                            }
+                        ?>
                         <li><a href="carro.php"><i class="material-icons left tooltipped" data-position="bottom" data-delay="50" data-tooltip="Carro de Compras">shopping_cart</i><span class="hide-on-med-and-down">Carro</span></a></li>
                     </ul>
                 </div>
