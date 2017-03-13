28ae840d44cbc447d205a44260dcc5d444cb9c53
Manuel Albani <mnlalbani@gmail.com>
2017-03-13 03:33:00 -0400

    script and styles referenced locally

--- pago-perfil.php
+++ pago-perfil.php
@@ -10,8 +10,8 @@
 <head>
     <script type="text/javascript" src="js/jquery-3.1.1.min"></script>
 	<script type="text/javascript" src="js/main.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
+    <script src="js/materialize.min.js"></script>
+    <link rel="stylesheet" href="css/materialize.min.css">
     <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
     <meta charset="UTF-8">
